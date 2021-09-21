b0beb4fe7f7f534d2f60e4a1225dfc661b91314c
SC2021 <walterlaw19@gmail.com>
2021-09-21 01:48:57 -0400

    remove lines extra

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
 
-// router.use('/api', apiRoutes);
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-    res.status(404).end();
-})
-
-module.exports = router
+module.exports = router;
--- db/db.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS my_first_tech_blog;
-
-CREATE DATABASE my_first_tech_blog;
